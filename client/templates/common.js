eb863654f24da699887e12426234413fc9dfb367
John Grieco <Johnjgrieco@gmail.com>
2016-03-29 11:19:06 -0700

    Added workshop on load

--- client/templates/common.js
+++ client/templates/common.js
@@ -3,6 +3,9 @@ Meteor.subscribe('workshops');
 Meteor.subscribe('names');
 
 Meteor.startup(function(){
+    
+   Workshops.insert({title: 'Hot Topics and Legislative Update', creditValue: 2, code:1111}); 
+    
    AccountsEntry.config({
       homeRoute: '/sign-in',
       dashboardRoute: '/',
